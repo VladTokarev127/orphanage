0c0ad7c36fdb7cbd7fb81653ada026abd1c6b83f
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-01-26 20:45:27 +0500

    Index done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
- // JS - код
+	$('.header__menu').click(function() {
+		$(this).toggleClass('is-active');
+		$('body').toggleClass('is-fixed');
+		$('.header__info').slideToggle(300);
+	});
+
+	$('.history__video-link').magnificPopup({
+		fixedContentPos: false,
+		type: 'iframe',
+	});
+
+	const currentSwiper = new Swiper('.current__swiper', {
+		freeMode: true,
+		spaceBetween: 50,
+		slidesPerView: 2
+	});
 
 });
